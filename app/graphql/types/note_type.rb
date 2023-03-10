b857b6d208c0c7f447eff24e729aefe7c40289f5
Anas Ahmad <ahmadanas4490@gmail.com>
2023-03-10 18:50:21 +0500

    dummy notes data

--- app/graphql/types/note_type.rb
+++ app/graphql/types/note_type.rb
@@ -3,8 +3,7 @@
 module Types
   class NoteType < Types::BaseObject
     field :id, ID, null: false
-    field :email, String, null: false
-    field :first_name, String, null: false
-    field :last_name, String, null: false
+    field :title, String, null: false
+    field :body, String, null: false
   end
 end
