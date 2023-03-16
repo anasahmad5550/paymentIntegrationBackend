aa1e25278d28c0ba499cd7a8c44079796737868d
Anas Ahmad <ahmadanas4490@gmail.com>
2023-03-16 15:39:09 +0500

    authenticate user with graphql

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-end
+    has_secure_password
+  
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+  end
